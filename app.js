49f9267076ebc662ccc8250f3dca9de5a4a1d43d
Franklin Aidoo <faidoo@IM-21-B1-400.local>
2024-11-18 13:05:01 +0000

    pre middleware

--- app.js
+++ app.js
@@ -22,14 +22,44 @@ app._router.get('/api/products/:productID', (req, res) => {
 
     const {productID} = req.params
 
-    const singleProduct = products.find((product) =>product.id === Number(productID))
+    const singleProduct = products.find( (product) =>product.id === Number(productID))
     if (!singleProduct){
         return res.status(404).json({success: false, msg: `No product with id ${productID}`})
     }else{
         return res.json(singleProduct)
     }
-    // res.json(singleProduct)
+    
 })
+
+app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
+    console.log(req.query)
+    res.send('Hello World')
+})
+app.get('/api/v1/query', (req,res) => {
+    console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if (search){
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        }
+    )
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        return res.status(200).json({success: true, data: []})
+    }
+
+    res.status(200).json(sortedProducts)
+    console.log(`Number of sorted Products would be ${limit}`)
+})
+
+
+
+
 
 
 // Listen for Requests
